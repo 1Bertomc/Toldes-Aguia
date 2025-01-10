c7a75f0dc73d60fa46ae13f58ed136978b49fc88
1Bertomc <61089906+1Bertomc@users.noreply.github.com>
2025-01-10 08:51:15 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,24 @@
+const CACHE_NAME = 'app-cache-v2';
+const urlsToCache = ['/', '/index.html', '/style.css', '/app.js'];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('app-cache').then(function(cache) {
-      return cache.addAll(['/', '/index.html', '/style.css', '/app.js']);
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cache) {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
