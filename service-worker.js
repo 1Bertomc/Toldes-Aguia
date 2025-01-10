6681ca17a05f7c0d9553b645bf85fa775fa20923
1Bertomc <61089906+1Bertomc@users.noreply.github.com>
2025-01-10 21:24:18 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,20 @@
-const CACHE_NAME = 'app-cache-v2';
-const urlsToCache = ['/', '/index.html', '/style.css', '/app.js'];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cache) {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('tenda-pwa-cache').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'style.css',
+                'script.js',
+                'manifest.json'
+            ]);
         })
-      );
-    })
-  );
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
